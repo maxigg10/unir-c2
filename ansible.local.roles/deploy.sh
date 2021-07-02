6a948e9655b0312bea4453d19afb802e24c089c6
Maximiliano <maximilianogadea@gmail.com>
2021-07-02 18:58:13 +0200

    ansible roles/playbooks v6

--- ansible.local.roles/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ansible-playbook -i hosts 01-playbook_requirements.yaml
-sleep 3m
-ansible-playbook -i hosts 02-playbook_NFS_installation.yaml
-ansible-playbook -i hosts 03-playbook_Master_Workers_commonTasks.yaml
-ansible-playbook -i hosts 04-playbook_Master_Configuration.yaml
-ansible-playbook -i hosts 05-playbook_Workers_Configuration.yaml
