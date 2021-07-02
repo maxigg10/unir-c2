7c96c7c3b3193260568d8dda9eb4fc4893ebbc89
Maximiliano <maximilianogadea@gmail.com>
2021-07-02 17:06:41 +0200

    ansible roles/playbooks v3

---
+++ ansible.local.roles/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# añadir tantas líneas como sean necesarias para el correcto despligue
+ansible-playbook -i hosts 01-playbook_requirements.yaml
+sleep (90)
+ansible-playbook -i hosts 02-playbook_NFS_installation.yaml
+ansible-playbook -i hosts 03-playbook_Master_Workers_commonTasks.yaml
+ansible-playbook -i hosts 04-playbook_Master_Configuration.yaml
+ansible-playbook -i hosts 05-playbook_Workers_Configuration.yaml
