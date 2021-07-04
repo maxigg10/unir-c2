1233ca517a6a13e2269966bf0b8ef240f208cc5f
Maximiliano <maximilianogadea@gmail.com>
2021-07-04 18:34:02 +0200

    Scripts

--- deploy_All_Azure.sh
+++ deploy_All_Azure.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
-echo -n "Despues de desplegar TERRAFORM debemos agregar las IPs publicas de AZURE en el archivo de hosts"
-echo -n "-------------------------------------"
-echo -n "-------------------------------------"
-echo -n "Desplegando TERRAFORM"
-sh ./terraform/deploy_terraform.sh
+echo  "Despues de desplegar TERRAFORM debemos agregar las IPs publicas de AZURE en el archivo de hosts "
+echo ""
+echo "-----------------------"
+echo  "Desplegando TERRAFORM"
+echo "-----------------------"
+#sh ./terraform/apply_terraform.sh
 
-echo -n ""
-echo -n "Debemos conectarnos a cada hosts"
-echo -n "ssh maxigg@master.ipxsistemas.com"
+echo ""
+
+echo "Agregaste las IPs al archivo de hosts y te conectaste a las maquinas? (si/no):   "
+read respuesta
+
+echo  " "
+echo "Debemos conectarnos a cada hosts"
+echo "ssh maxigg@master.ipxsistemas.com"
+echo " "
 ssh -o "StrictHostKeyChecking no" maxigg@master.ipxsistemas.com << EOF
  exit
 EOF
-echo -n "ssh maxigg@worker01.ipxsistemas.com"
+echo ""
+echo  "ssh maxigg@worker01.ipxsistemas.com"
+echo  " "
 ssh -o "StrictHostKeyChecking no" maxigg@worker01.ipxsistemas.com << EOF
  exit
 EOF
-echo -n "ssh maxigg@worker02.ipxsistemas.com"
+echo""
+echo  "ssh maxigg@worker02.ipxsistemas.com"
+echo  " "
 ssh -o "StrictHostKeyChecking no" maxigg@worker02.ipxsistemas.com << EOF
  exit
 EOF
-
-echo -n "ssh maxigg@nfs.ipxsistemas.com"
+echo ""
+echo  "ssh maxigg@nfs.ipxsistemas.com"
+echo  " "
 ssh -o "StrictHostKeyChecking no" maxigg@nfs.ipxsistemas.com << EOF
  exit
 EOF
-
-
-echo -n "Agregaste las IPs al archivo de hosts y te conectaste a las maquinas? (si/no):   "
-read respuesta
+echo ""
 case $respuesta in
 
   si)
-    echo -n "Desplegando ANSIBLE"
+    echo "*****************"   
+    echo  "Desplegando ANSIBLE"
+    echo "*****************"
     sh ./ansible/deploy_ansible.sh
     ;;
 
   no)
-    echo -n "No se ejecuta nada"
+    echo  "No se ejecuta nada"
     exit
     ;;
 
---
+++ destroy_All_Azure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo ""
+echo "-----------------------"
+echo  "Destruyendo Infraestura de TERRAFORM"
+echo "-----------------------"
+sh ./terraform/destroy_terraform.sh
+
